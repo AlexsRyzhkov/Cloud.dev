9dd7d366785b1a37f843d74ef4569a5cfc9d0a43
Timur <MuratovTim3@gmail.com>
2024-05-25 12:47:33 +0300

    feat(#no_number): Добавлен UserView

--- backend/cloud_dev/authentication/urls.py
+++ backend/cloud_dev/authentication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("refresh/", views.RefreshTokenView.as_view(), name="refresh_token"),
+    path("user/", views.UserInfoView.as_view(), name="user"),
 ]
